82d46375bed1af1ec43da59bcfef7ef2889d943f
muzenjakkevin <muzenjak57@gmail.com>
2021-04-22 19:18:02 +0200

    changed nav to styled components

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,28 +1,56 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 import logotype from '../../images/logotype.png';
+import Logo from './logo/Logo';
 import './Navbar.css';
+
+const NavbarContainer = styled.div`
+  background-color: #3b4250;
+  position: sticky;
+  width: 100%;
+  height: 11em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+  box-shadow: 0 4px 6px 0 rgba(0, 0, 0, 0.3);
+`;
+
+const NavbarLeftSection = styled.div`
+  display: flex;
+`;
+
+const NavbarRightSection = styled.div`
+  display: flex;
+`;
 
 function Navbar() {
 
   return (
-    <nav className='nav'>
-      <img className='logo' src={logotype} alt="logotype"/>
-      <ul className='nav-menu'>
-        <Link className="nav-links" to='/'>
-          <li>Home</li>
-        </Link>
-        <Link className="nav-links" to='/about'>
-          <li>About</li>
-        </Link>
-        <Link className="nav-links" to='/news'>
-          <li>News</li>
-        </Link>
-        <Link className="nav-links" to='/contact'>
-          <li>Contact</li>
-        </Link>
-      </ul>
-    </nav>
+    <>
+      <NavbarContainer>
+        <NavbarLeftSection>
+          <Logo/>
+        </NavbarLeftSection>
+        <NavbarRightSection>
+          <ul className='nav-menu'>
+            <Link className="nav-links" to='/'>
+              <li>Home</li>
+            </Link>
+            <Link className="nav-links" to='/about'>
+              <li>About</li>
+            </Link>
+            <Link className="nav-links" to='/news'>
+              <li>News</li>
+            </Link>
+            <Link className="nav-links" to='/contact'>
+              <li>Contact</li>
+            </Link>
+          </ul>
+        </NavbarRightSection>
+      </NavbarContainer>
+    </>
   )
 }
 
---
+++ src/components/Navbar/logo/Logo.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from 'styled-components'
+import logo from '../../../images/logotype.png'
+
+const LogoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const LogoImage = styled.div`
+  width: 10%;
+  margin-left: 4em;
+  img{
+      width: 100%;
+  }
+`;
+
+export default function Logo() {
+  return (
+    <>
+      <LogoWrapper>
+        <LogoImage><img src={logo} alt="Lamborghini"/></LogoImage>
+      </LogoWrapper>
+    </>
+  )
+}
