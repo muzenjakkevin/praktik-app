26fa100af5081bb0d54f8658c2999fea4e1b1187
muzenjakkevin <muzenjak57@gmail.com>
2021-04-27 15:26:21 +0200

    changed the article error screen

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -42,7 +42,7 @@ function Articles() {
         
         : 
         <div className="error-container">
-          <h4 id="error-text">There was an error trying to fetch the news</h4>
+          <h4 id="error-text">Error fetching article</h4>
         </div>
         }
       </div>
